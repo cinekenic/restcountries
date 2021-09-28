d627e04c0a96c765604e739d15f1f3b3fe800877
Piotr-Trzcinski <cinekenic@go2.pl>
2021-09-28 12:20:34 +0200

    checking if data exists

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
 const allCountysKey = "Countries";
-fetch(
-  "http://api.countrylayer.com/v2/all?access_key=25dd99021bdb701631851215e7885201"
-)
-  .then((res) => res.json())
-  .then((res) => {
-    const data = res;
-    console.log(res);
-    localStorage.setItem(allCountysKey, JSON.stringify(data));
-  });
+
+function dataFromFetch() {
+  fetch(
+    "http://api.countrylayer.com/v2/all?access_key=25dd99021bdb701631851215e7885201"
+  )
+    .then((res) => res.json())
+    .then((res) => {
+      const data = res;
+      // console.log(res);
+      localStorage.setItem(allCountysKey, JSON.stringify(data));
+    });
+}
+
+function checkExistDataFromFetch() {
+  if (!localStorage.getItem("Countries")) {
+    dataFromFetch();
+  }
+}
+
+checkExistDataFromFetch();
