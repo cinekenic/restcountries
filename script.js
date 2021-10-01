7de92abe50dbcc97585d6818ad2ff7eddb094264
Piotr-Trzcinski <cinekenic@go2.pl>
2021-10-01 14:19:44 +0200

    bug fix

--- script.js
+++ script.js
@@ -5,25 +5,27 @@ const sevenDays = 604800000;
 const time = new Date().getTime();
 
 function filterCountries(countries) {
-  console.log(
-    countries
-      .filter(
-        (el) =>
-          el.regionalBlocs &&
-          el.regionalBlocs[0].name.includes("European Union")
-      )
-      .filter((el) => !el.name.includes("a"))
-      .sort((a, b) => {
-        a.population - b.population;
-      })
-      .reverse()
-  );
+  countries = countries
+    .filter(
+      (el) =>
+        el.regionalBlocs &&
+        el.regionalBlocs.find((findEl) =>
+          findEl.name.includes("European Union")
+        )
+    )
+    .filter((el) => !el.name.includes("a"))
+    .sort((a, b) => a.population - b.population)
+    .reverse();
+  console.log(countries);
 }
 
 function comparison(data, prevData) {
   console.log("func comparison is runing");
-  const ids = prevData.map((e) => e.population);
-  let filtered = data.filter((e) => !ids.includes(e.population));
+  let filtered = data.filter((el) => {
+    const prevCountryData = prevData.find((prevEl) => prevEl.name === el.name);
+    return el.population !== prevCountryData.population;
+  });
+
   for (let el of filtered) {
     console.log(el.name);
   }
@@ -32,33 +34,26 @@ function comparison(data, prevData) {
 function dataFromFetch() {
   fetch(urlApi)
     .then((res) => res.json())
-    .then((res) => {
-      const data = res;
+    .then((data) => {
+      const prevData = JSON.parse(localStorage.getItem(allCountysKey));
 
       localStorage.setItem(allCountysKey, JSON.stringify(data));
-
-      const prevData = JSON.parse(localStorage.getItem(allCountysKey));
       localStorage.setItem(lastTime, JSON.stringify(new Date().getTime()));
 
-      comparison(data, prevData);
+      if (prevData) {
+        comparison(data, prevData);
+      }
       filterCountries(data);
     });
 }
 
-function checkExistDataFromFetch() {
-  if (!localStorage.getItem("Countries")) {
+function checkDataFromFetch() {
+  if (
+    !localStorage.getItem("Countries") ||
+    time - localStorage.getItem(lastTime) > sevenDays
+  ) {
     dataFromFetch();
   }
 }
 
-checkExistDataFromFetch();
-
-function downladAfterSevenDays() {
-  let newTime = time - localStorage.getItem("lastTime");
-  if (newTime > sevenDays) {
-    console.log("data download after seven days");
-    dataFromFetch();
-  }
-}
-
-downladAfterSevenDays();
+checkDataFromFetch();
